fafdd21bc03f60c8dd7a53c8733df52e87fc15df
mikael.bezerra950@gmail.com <mikael.bezerra950@gmail.com>
2021-03-22 21:12:32 -0300

    first commit

---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const demonmenu = (prefix, pushname) => {
+    return `◪ *Comandos do demon*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.demonmenu = demonmenu
